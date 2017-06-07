a0e2e83260dee763f257f4d7812992bd8a44ae77
Simon Wollwage <mail.wollwage@gmail.com>
2017-06-07 13:05:20 +0900

    test: add test cases

--- src/lib.rs
+++ src/lib.rs
@@ -91,3 +91,49 @@ impl<A, B> Sink for RouterSink<A, B>
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::{Route, RouterSink, RouterSinkError};
+    use futures::{Future, stream, Stream};
+
+    #[test]
+    fn poll_all() {
+        let a: Vec<u32> = Vec::new();
+        let b: Vec<u32> = Vec::new();
+
+        let input: Vec<Result<_, ()>> = vec![Ok(Route::Left(23)), Ok(Route::Right(42))];
+        let stream = stream::iter(input);
+
+        let router = RouterSink::new(a, b);
+
+        match stream
+                  .map_err(|_| RouterSinkError::Left(()))
+                  .forward(router)
+                  .wait() {
+            Ok((_, router)) => {
+                assert_eq!(router.left_sink[0], 23);
+                assert_eq!(router.right_sink[0], 42);
+            }
+            Err(_) => assert!(false),
+        }
+    }
+
+    #[test]
+    fn poll_err() {
+        let a: Vec<u32> = Vec::new();
+        let b: Vec<u32> = Vec::new();
+
+        let input = vec![Ok(Route::Left(23)), Err(())];
+        let stream = stream::iter(input);
+
+        let router = RouterSink::new(a, b);
+
+        assert_eq!(true,
+                   stream
+                       .map_err(|_| RouterSinkError::Left(()))
+                       .forward(router)
+                       .wait()
+                       .is_err());
+    }
+}
